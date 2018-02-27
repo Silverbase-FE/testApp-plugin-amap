97d9b62e5e0a175856a35dd496d06dcf81c30ad2
杨能康 <nk_yang@silverbase.com.cn>
2018-02-27 08:59:27 +0800

    demo for amap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,31 +34,38 @@ export class MyApp {
 
       function onDeviceReady() {
         alert(JSON.stringify(device));
-        alert(JSON.stringify(cordova));
-        alert(typeof networkinterface);
-        alert(typeof AMapPlugin);
-        if(typeof networkinterface != 'undefined'){
+        if (typeof networkinterface != 'undefined') {
           console.log(networkinterface.getWiFiIPAddress);
-  　　　　　　networkinterface.getWiFiIPAddress(function(ip){
-  　　　　　　　　alert(ip);
-  　　　　　　});
-  　　　　}
-
-        if (typeof AMapPlugin != 'undefined') {
-          console.log(AMapPlugin.startUpdatePosition);
-          AMapPlugin.startUpdatePosition(function (data) {
-            alert("success" + JSON.stringify(data));
-
-            if (data == 200) {
-              AMapPlugin.readUpdatePosition(function(data) {
-                alert("success" + JSON.stringify(data));
-              }, function(err) {
-                alert("error" + JSON.stringify(err));
-              });
-            }
-          }, function(err) {
-            alert("err" + JSON.stringify(err));
+          networkinterface.getWiFiIPAddress(function (ip) {
+            alert(ip);
           });
+        }
+
+        // 持续定位
+        // if (typeof AMapPlugin != 'undefined') {
+        //   console.log(AMapPlugin.startUpdatePosition);
+        //   AMapPlugin.startUpdatePosition(function (data) {
+        //     alert("data1" + JSON.stringify(data));
+        //   }, function(err) {
+        //     alert("err1" + JSON.stringify(err));
+        //   });
+
+        //   AMapPlugin.readUpdatePosition(function(data) {
+        //     alert("success2" + JSON.stringify(data));
+        //   }, function(err) {
+        //     alert("err2" + JSON.stringify(err));
+        //   })
+        // }
+
+        // 单次定位
+        if (typeof AMapPlugin != 'undefined') {
+          setInterval(function () {
+            AMapPlugin.getCurrentPosition(function (data) {
+              alert("data" + JSON.stringify(data));
+            }, function (err) {
+              alert("err" + JSON.stringify(err));
+            })
+          }, 5000);
         }
       }
 
